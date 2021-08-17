c90abff56e8daf8533b3e56d4e548b27f92ce674
Chris Armstrong <mail@chrisarmstrong.io>
2021-08-17 17:32:36 +0100

    Application journey
    First couple of sections added

---
+++ app/views/application/v1/_routes.js
@@ -0,0 +1,236 @@
+const express = require('express')
+const router = new express.Router()
+
+// Set variables
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  // res.locals['serviceName'] = 'Check if you can get a Gender Recognition Certificate'
+
+  // Change the service name URL so it links back this features index
+  res.locals['serviceUrl'] = '/'+req.originalUrl.split('/')[1]+'/'
+
+  next()
+})
+
+// Clear session data on start page
+router.get('/start', function (req, res, next) {
+  req.session.destroy()
+  next()
+})
+
+
+
+/////////////////////////////////////////////////////
+// ELIGIBILITY
+//
+router.post('/eligibility/age-check', function (req, res) {
+  if (req.session.data['age-check'] == 'No') {
+    res.redirect('not-old-enough');
+  } else {
+    res.redirect('overseas-check');
+  }
+})
+
+router.post('/eligibility/overseas-check', function (req, res) {
+  if (req.session.data['overseas-check'] == 'Yes') {
+    res.redirect('overseas-approved-check');
+  } else {
+    res.redirect('dysphoria-check');
+  }
+})
+
+router.post('/eligibility/overseas-approved-check', function (req, res) {
+  if (req.session.data['overseas-approved-check'] == 'Yes') {
+    res.redirect('overseas-track');
+  } else {
+    res.redirect('dysphoria-check');
+  }
+})
+
+router.post('/eligibility/dysphoria-check', function (req, res) {
+  if (req.session.data['dysphoria-check'] == 'No') {
+    res.redirect('dysphoria-no');
+  } else {
+    res.redirect('two-years-check');
+  }
+})
+
+router.post('/eligibility/two-years-check', function (req, res) {
+  if (req.session.data['two-years-check'] == 'No') {
+    res.redirect('two-years-no');
+  } else {
+    res.redirect('until-death-check');
+  }
+})
+
+router.post('/eligibility/until-death-check', function (req, res) {
+  if (req.session.data['until-death-check'] == 'No') {
+    res.redirect('until-death-no');
+  } else {
+    res.redirect('../marriage-civil-partnership/current-check');
+  }
+})
+//
+// ELIGIBILITY
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////
+// MARRIAGE / CIVIL PARTNERSHIP
+//
+router.post('/marriage-civil-partnership/current-check', function (req, res) {
+  if (req.session.data['current-check'] == 'Neither') {
+    res.redirect('partner-died');
+  } else if (req.session.data['current-check'] == 'Civil partnership') {
+    res.redirect('protected-civil-partnership');
+  } else {
+    res.redirect('protected-marriage');
+  }
+})
+
+router.post('/marriage-civil-partnership/partner-died', function (req, res) {
+  res.redirect('../personal/name');
+})
+
+
+
+
+
+// MARRIAGE
+router.post('/marriage-civil-partnership/protected-marriage', function (req, res) {
+  if (req.session.data['protected-marriage'] == 'No') {
+    res.redirect('interim-check');
+  } else {
+    res.redirect('stay-together');
+  }
+})
+
+router.post('/marriage-civil-partnership/stay-together', function (req, res) {
+  if (req.session.data['stay-together'] == 'No') {
+    res.redirect('interim-check');
+  } else {
+    res.redirect('../personal/name');
+  }
+})
+
+
+
+
+//CIVIL PARTNERSHIP
+router.post('/marriage-civil-partnership/protected-civil-partnership', function (req, res) {
+  if (req.session.data['protected-civil-partnership'] == 'No') {
+    res.redirect('interim-check');
+  } else if (req.session.data['protected-civil-partnership'] == 'Yes, a protected Scottish civil partnership') {
+    res.redirect('same-time');
+  } else {
+    res.redirect('stay-together');
+  }
+})
+
+router.post('/marriage-civil-partnership/same-time', function (req, res) {
+  if (req.session.data['same-time'] == 'No') {
+    res.redirect('interim-check');
+  } else {
+    res.redirect('../personal/name');
+  }
+})
+
+
+
+
+router.post('/marriage-civil-partnership/stay-together', function (req, res) {
+  if (req.session.data['stay-together'] == 'No') {
+    res.redirect('interim-check');
+  } else {
+    res.redirect('../personal/name');
+  }
+})
+
+router.post('/marriage-civil-partnership/interim-check', function (req, res) {
+  if (req.session.data['interim-check'] == 'No') {
+    res.redirect('interim-no');
+  } else {
+    res.redirect('../personal/name');
+  }
+})
+//
+// MARRIAGE / CIVIL PARTNERSHIP
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////
+// PERSONAL DETAILS
+//
+router.post('/personal/name', function (req, res) {
+  res.redirect('another-name-check');
+})
+
+router.post('/personal/another-name-check', function (req, res) {
+  res.redirect('address');
+})
+
+router.post('/personal/address', function (req, res) {
+  res.redirect('contact-preferences');
+})
+
+router.post('/personal/contact-preferences', function (req, res) {
+  res.redirect('hmrc');
+})
+
+router.post('/personal/hmrc', function (req, res) {
+  if (req.session.data['notify-hmrc'] == 'No') {
+    res.redirect('../birth-adoption/name');
+  } else {
+    res.redirect('ni-number');
+  }
+})
+
+router.post('/personal/ni-number', function (req, res) {
+  res.redirect('../birth-adoption/name');
+})
+//
+// PERSONAL DETAILS
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////
+// BIRTH / ADOPTION
+//
+
+//
+// BIRTH / ADOPTION
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////
+// PAYMENT
+//
+
+//
+// PAYMENT
+/////////////////////////////////////////////////////
+
+
+router.post('/eligibility/declaration', function (req, res) {
+  res.redirect('task-list');
+})
+
+
+
+
+
+
+// Add your routes above the module.exports line
+module.exports = router
